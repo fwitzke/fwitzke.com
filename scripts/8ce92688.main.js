c501a6c1d5a4c13c17a69ee38590f1f664156b0e
Fernando Witzke <pigroxalot@gmail.com>
2013-11-23 21:08:05 -0200

    publish: making sure no dirty html is displayed on loading
    generated from commit 4c2a0ebe3f1b5007fe01b8decbf37d639c3c3431

--- scripts/8ce92688.main.js
+++
@@ -1,1 +0,0 @@
-"use strict";var app=angular.module("witzke",[]);app.controller("ExperienceCtrl",["$scope","$http","$location",function(a,b,c){a.experiences=[];var d=c.absUrl()+"experiences.json";b.get(d).success(function(b){a.experiences=b})}]),app.filter("period",function(){return function(a){return a.start+" - "+a.end}});
