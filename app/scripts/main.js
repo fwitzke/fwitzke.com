4c2a0ebe3f1b5007fe01b8decbf37d639c3c3431
Fernando Witzke <pigroxalot@gmail.com>
2013-11-23 21:07:39 -0200

    making sure no dirty html is displayed on loading

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,26 @@
 'use strict';
 var app = angular.module('witzke', []);
 
-app.controller('ExperienceCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
+app.controller('ExperienceCtrl', ['$scope', '$http', '$location', '$filter', function($scope, $http, $location, $filter) {
   $scope.experiences = [];
 
   var jsonUrl = $location.absUrl() + 'experiences.json';
   $http.get(jsonUrl).success(function(data){
     $scope.experiences = data;
   });
+
+  $scope.getMeta = function(item) {
+    return $filter('period')(item.period) + ' | ' + item.location;
+  };
+
+  $scope.getClass = function(item) {
+    if(item.avatar) {
+      return 'avatar ' + item.avatar;
+    } else {
+      return 'image ' + item.image;
+    }
+  };
+
 }]);
 
 app.filter('period', function(){
